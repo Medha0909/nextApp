ccaf7b75e68eae81aac28291dd2ee35585ede9df
Medha0909 <103240973+Medha0909@users.noreply.github.com>
2024-07-11 04:42:01 -0700

    Add files via upload

---
+++ client/pages/signin.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function signin() {
+  return (
+    <div>
+      <input placeholder="Continue with Google"></input>
+      <input placeholder="Continue with Facebook"></input>
+
+    </div>
+  )
+}
+
+export default signin
